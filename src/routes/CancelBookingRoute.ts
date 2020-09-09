2ee3836d873243a44a7e711f23ffb4bd2cafccb9
leoyn <leo@mixify.ga>
2020-09-10 01:25:50 +0200

    Add missing Ticket import

--- src/routes/CancelBookingRoute.ts
+++ src/routes/CancelBookingRoute.ts
@@ -1,5 +1,6 @@
 import { Route } from "./Route";
 import { TicketShopImplementation } from "com.cinecar.ticketshop";
+import { Ticket } from "com.cinecar.objects";
 
 export class CancelBookingRoute implements Route {
     handle(req: any, res: any): void {
