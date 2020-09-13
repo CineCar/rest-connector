02223cb0c92b0d77205a2b8120cb2506404daa1e
leoyn <leo@mixify.ga>
2020-09-13 19:31:58 +0200

    Get params from request in DeleteTicketFromCartRoute

--- src/routes/DeleteTicketFromCartRoute.ts
+++ src/routes/DeleteTicketFromCartRoute.ts
@@ -4,7 +4,7 @@ import { Route } from "./Route";
 export class DeleteTicketFromCartRoute implements Route {
     handle(req: any, res: any): void {
         TicketShopImplementation.getSingleton()
-            .deleteTicketFromCart(parseInt(res.params.cartId), parseInt(res.params.ticketId))
+            .deleteTicketFromCart(parseInt(req.params.cartId), parseInt(req.params.ticketId))
             .then((cart) => {
                 res.api.data(cart.toJSON());
             })
